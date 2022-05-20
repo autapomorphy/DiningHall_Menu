e6a9a6c412f79149a3c677f4c6ce81c722cfe7bf
Autapomorphy <autapomorphy1995@gmail.com>
2022-05-19 22:19:04 -0400

    Project initialized: spring initializr based on Spring Framework 5: Beginner to Guru

---
+++ DiningHall_Menu/src/main/java/auta/website/dininghall_menu/DiningHallMenuApplication.java
@@ -0,0 +1,13 @@
+package auta.website.dininghall_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningHallMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningHallMenuApplication.class, args);
+	}
+
+}
---
+++ DiningHall_Menu/src/test/java/auta/website/dininghall_menu/DiningHallMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package auta.website.dininghall_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningHallMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
