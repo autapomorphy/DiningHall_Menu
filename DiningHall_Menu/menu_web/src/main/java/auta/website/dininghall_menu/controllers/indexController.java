c82cec429073b5a5197fdaa8ab5bcdc141c3be39
Autapomorphy <autapomorphy1995@gmail.com>
2022-05-26 15:54:15 -0400

    Thymeleaf index template and service

---
+++ DiningHall_Menu/menu_web/src/main/java/auta/website/dininghall_menu/controllers/indexController.java
@@ -0,0 +1,12 @@
+package auta.website.dininghall_menu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
